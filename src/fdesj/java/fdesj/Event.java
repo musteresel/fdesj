ac375c6f64c90138f89e7db9a60af36469b2c055
Daniel Jour <musteresel@gmail.com>
2016-12-18 01:15:53 +0100

    gradle build infrastructure

---
+++ src/fdesj/java/fdesj/Event.java
@@ -0,0 +1,6 @@
+package fdesj;
+
+
+public interface Event
+{
+}
---
+++ src/unit/java/fdesj/EventTest.java
@@ -0,0 +1,14 @@
+package fdesj;
+
+
+import org.junit.Test;
+
+
+public class EventTest
+{
+  @Test
+  public void createAnonymous()
+  {
+    new Event() {};
+  }
+}
