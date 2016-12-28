c181ac689a46893fcac142cd47be213ecf54d726
Daniel Jour <musteresel@gmail.com>
2016-12-28 22:19:05 +0100

    add @Override annotation

--- src/unit/java/fdesj/TimeComparableTest.java
+++ src/unit/java/fdesj/TimeComparableTest.java
@@ -5,16 +5,19 @@ package fdesj;
  * Test comparable implementation of Time class.
  */
 public class TimeComparableTest extends AbstractComparableTest<Time> {
+  @Override
   public Time createLowValue() {
     return new Time(0);
   }
 
 
+  @Override
   public Time createMidValue() {
     return new Time(21);
   }
 
 
+  @Override
   public Time createHighValue() {
     return new Time(42);
   }
