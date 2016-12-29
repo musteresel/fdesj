0fb80d837cf28558c1fe2f84531129df7f3fd408
Daniel Jour <musteresel@gmail.com>
2016-12-29 14:33:09 +0100

    collect all changes in a class

---
+++ src/fdesj/java/fdesj/Changes.java
@@ -0,0 +1,32 @@
+package fdesj;
+
+
+/**
+ * Sum of the changes which can be induced by an event.
+ */
+public final class Changes<State> {
+  /**
+   * Changes that act on the simulation state.
+   */
+  public final StateChanges<State> toState;
+
+
+  /**
+   * Changes that act on the event list of the simulation.
+   */
+  public final Occurrences.Changes toOccurrences;
+
+
+  /**
+   * Construct with the given changes to parts of the simulation.
+   *
+   * @param toState Changes which apply to the simulation state.
+   * @param toOccurrences Changes to the event list.
+   */
+  public Changes(final StateChanges<State> toState,
+                 final Occurrences.Changes toOccurrences) {
+    // TODO: Decide whether null should be banned.
+    this.toState = toState;
+    this.toOccurrences = toOccurrences;
+  }
+}
