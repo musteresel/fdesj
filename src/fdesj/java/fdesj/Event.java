3e5ab981640e8e59ab3fe4f18da5aa6638ae6571
Daniel Jour <musteresel@gmail.com>
2016-12-30 01:41:29 +0100

    events affect the simulation

--- src/fdesj/java/fdesj/Event.java
+++ src/fdesj/java/fdesj/Event.java
@@ -5,4 +5,16 @@ package fdesj;
  * An event in the simulation.
  */
 public interface Event<State> {
+  /**
+   * Describe how the event affects the simulation.
+   *
+   * Calling this function multiple times with equal arguments must
+   * return equal results.
+   *
+   * @param state Simulation state.
+   * @param at Time at which the event has its effect.
+   * @return Description of the changes to the simulation induced by
+   * the event.
+   */
+  Changes<State> affect(State state, Time at);
 }
--- src/unit/java/fdesj/DummyEvent.java
+++ src/unit/java/fdesj/DummyEvent.java
@@ -5,4 +5,9 @@ package fdesj;
  * Dummy event class used by testing code.
  */
 public final class DummyEvent implements Event<Object> {
+  @Override
+  public Changes<Object> affect(final Object state, final Time at) {
+    // TODO: This is ugly, provide a way to say "no changes".
+    return new Changes<Object>(null, null);
+  }
 }
